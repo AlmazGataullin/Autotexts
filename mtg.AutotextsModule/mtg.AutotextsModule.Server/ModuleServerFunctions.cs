d8cfaba17e82e5d158868a9bda47cda87b250a30
danilazyan95 <gohurot@mail.ru>
2023-06-21 16:49:03 +0300

    Добавлена функция, возвращающая список автотекстов, для последующего использования в перекрытиях - задачах, заданиях

--- mtg.AutotextsModule/mtg.AutotextsModule.Server/ModuleServerFunctions.cs
+++ mtg.AutotextsModule/mtg.AutotextsModule.Server/ModuleServerFunctions.cs
@@ -8,6 +8,16 @@ namespace mtg.AutotextsModule.Server
 {
   public class ModuleFunctions
   {
-    
+    /// <summary>
+    /// Получить список автотекстов с учетом области использования.
+    /// </summary>
+    /// <param name="usageAreaGuid">Guid области использования.</param>
+    [Public, Remote]
+    public static IQueryable<IAutotext> GetAutotexts(string usageAreaGuid)
+    {
+      var query = Autotexts.GetAll(x => x.Status == mtg.AutotextsModule.Autotext.Status.Active && Equals(x.UsageArea.Guid, usageAreaGuid));
+      
+      return query;
+    }
   }
 }
